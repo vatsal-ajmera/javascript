e4b3d1b8efcb3c6e36eafeced1be74c2c677001c
Vatsal <vatsal@zealousweb.com>
2024-12-10 15:08:55 +0530

    Learning JS

---
+++ dom-elements/scripts/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Selectors
+// let elements = document.getElementsByTagName('div');
+// elements = document.getElementsByClassName('ul-main');
+// elements = document.getElementById('main-div')
+// let element = document.querySelector('.item');
+// console.log(element);
+// let elements = document.querySelectorAll('.item');
+// console.log(elements)
+
+
+    // Child and parents
+// let parentElement = document.querySelector('.ul-main').parentElement;
+// console.log(parentElement.children)
+// console.log(parentElement.childNodes) 
+
+// Append the element
+// let ulElement = document.querySelector('.ul-main')
+// let newLi = document.createElement('li')
+// newLi.textContent = 'new Li'
+// newLi.classList.add('item')
+// ulElement.appendChild(newLi)
+
+// console.log(ulElement.firstElementChild.remove())
+// console.log(ulElement.lastElementChild.remove())
+
+
+
+});
+
+
+
